c7ca255377ab250146c7a44334932955c6b5fa18
savepong <savepong.com@gmail.com>
2020-01-19 23:12:29 +0700

    Loading Photos

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,10 +11,29 @@ import {
   FilesystemDirectory
 } from "@capacitor/core";
 
+const PHOTO_STORAGE = "photos";
+
 export function usePhotoGallery() {
+  const { get, set } = useStorage();
   const { getPhoto } = useCamera();
   const [photos, setPhotos] = useState<Photo[]>([]);
   const { deleteFile, getUri, readFile, writeFile } = useFilesystem();
+
+  useEffect(() => {
+    const loadSaved = async () => {
+      const photosString = await get("photos");
+      const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+      for (let photo of photos) {
+        const file = await readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+        });
+        photo.base64 = `data:image/jpeg;base64,${file.data}`;
+      }
+      setPhotos(photos);
+    };
+    loadSaved();
+  }, [get, readFile]);
 
   const takePhoto = async () => {
     const cameraPhoto = await getPhoto({
@@ -27,6 +46,19 @@ export function usePhotoGallery() {
     const savedFileImage = await savePicture(cameraPhoto, fileName);
     const newPhotos = [savedFileImage, ...photos];
     setPhotos(newPhotos);
+
+    set(
+      PHOTO_STORAGE,
+      JSON.stringify(
+        newPhotos.map(p => {
+          // Don't save the base64 representation of the photo data,
+          // since it's already saved on the Filesystem
+          const photoCopy = { ...p };
+          delete photoCopy.base64;
+          return photoCopy;
+        })
+      )
+    );
   };
 
   const savePicture = async (photo: CameraPhoto, fileName: string) => {
