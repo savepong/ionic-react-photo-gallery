cac34ab54aac1a249b6a36e0e4f14c1c1220a27d
savepong <savepong.com@gmail.com>
2020-01-20 00:08:08 +0700

    delete photo

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { camera, trash, close } from "ionicons/icons";
 import {
   IonContent,
@@ -15,10 +15,11 @@ import {
   IonImg,
   IonActionSheet
 } from "@ionic/react";
-import { usePhotoGallery } from "../hooks/usePhotoGallery";
+import { usePhotoGallery, Photo } from "../hooks/usePhotoGallery";
 
 const Tab2: React.FC = () => {
-  const { photos, takePhoto } = usePhotoGallery();
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<Photo>();
   return (
     <IonPage>
       <IonHeader>
@@ -31,7 +32,10 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.base64 ?? photo.webviewPath} />
+                <IonImg
+                  onClick={() => setPhotoToDelete(photo)}
+                  src={photo.base64 ?? photo.webviewPath}
+                />
               </IonCol>
             ))}
           </IonRow>
@@ -42,6 +46,29 @@ const Tab2: React.FC = () => {
             <IonIcon icon={camera}></IonIcon>
           </IonFabButton>
         </IonFab>
+
+        <IonActionSheet
+          isOpen={!!photoToDelete}
+          buttons={[
+            {
+              text: "Delete",
+              role: "destructive",
+              icon: trash,
+              handler: () => {
+                if (photoToDelete) {
+                  deletePhoto(photoToDelete);
+                  setPhotoToDelete(undefined);
+                }
+              }
+            },
+            {
+              text: "Cancel",
+              icon: close,
+              role: "cancel"
+            }
+          ]}
+          onDidDismiss={() => setPhotoToDelete(undefined)}
+        />
       </IonContent>
     </IonPage>
   );
